d8e1590462e889ed70028e13e1805fc44e1577a8
ShaknaRizath <shakna48@gmail.com>
2026-08-08 16:57:25 +0530

    Add suppressHydrationWarning attribute to html element for improved hydration handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${bodyFont.variable} ${headingFont.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
         <TooltipProvider>{children}</TooltipProvider>
